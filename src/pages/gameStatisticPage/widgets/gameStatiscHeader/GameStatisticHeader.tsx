c56d52f69c08b535c7078cbe7e302965e933eda0
Aleksey Minigaleev <Aleksey.Minigaleev@gmail.com>
2024-03-15 22:52:50 +0500

    #42 add layout

--- src/pages/gameStatisticPage/widgets/gameStatiscHeader/GameStatisticHeader.tsx
+++ src/pages/gameStatisticPage/widgets/gameStatiscHeader/GameStatisticHeader.tsx
@@ -8,33 +8,44 @@ interface GameStatisticHeaderProps {
 export default function GameStatisticHeader(props: GameStatisticHeaderProps) {
   return (
     <>
-      <div className={classes["preview"]}>
-        <img
-          className={classes["img"]}
-          src={props.game.previewURL}
-          width={275}
-          height={175}
-        />
-      </div>
+      <div className={classes["wrapper"]}>
+        <div className={classes["game-container"]}>
+          <div className={classes["preview"]}>
+            <img
+              className={classes["img"]}
+              src={props.game.previewURL}
+              width={275}
+              height={175}
+            />
+          </div>
 
-      <div>
-        <p className={classes["title"]}>{props.game.name}</p>
+          <div>
+            <p className={classes["title"]}>{props.game.name}</p>
 
-        <div className={classes["categories"]}>
-          {props.game.categories.map((category, index) => (
-            <span className={classes["category"]} key={index}>
-              {category}
-            </span>
-          ))}
-        </div>
+            <div className={classes["categories"]}>
+              {props.game.categories.map((category, index) => (
+                <span className={classes["category"]} key={index}>
+                  {category}
+                </span>
+              ))}
+            </div>
 
-        <div
-          className={classes["game-link"]}
-          onClick={() => (window.location.href = props.game.url)}
-        >
-          <a href={props.game.url} className={classes["link"]}>
-            Перейти
-          </a>
+            <div
+              className={classes["game-link"]}
+              onClick={() => (window.location.href = props.game.url)}
+            >
+              <a href={props.game.url} className={classes["link"]}>
+                Перейти
+              </a>
+            </div>
+          </div>
+
+          <div className={classes["pageId-wrapper"]}>
+            <div className={classes["pageId-container"]}>
+              <input placeholder="pageId" />
+              <button>установить</button>
+            </div>
+          </div>
         </div>
       </div>
     </>
