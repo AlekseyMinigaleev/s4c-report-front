61df0df87a7ae1e587bcc55901a30325bee235aa
Aleksey Minigaleev <Aleksey.Minigaleev@gmail.com>
2024-03-29 12:30:06 +0500

    #54 add barloader on signUp button

--- src/pages/authPages/widgets/LogIn-form.tsx
+++ src/pages/authPages/widgets/LogIn-form.tsx
@@ -17,6 +17,7 @@ import ForgotPassword from "../components/forgotPassword/ForgotPassword";
 import classes from "./form.module.css";
 import { round } from "lodash";
 import { routeType } from "models/routeType";
+import { BarLoader } from "react-spinners";
 
 export default function LogInForm() {
   const [serverErrorMessage, setServerErrorMessage] = useState<string>("");
@@ -91,7 +92,7 @@ export default function LogInForm() {
         isActive={isValidFormForRequest}
         disabled={!isValidFormForRequest || isLoading}
       >
-        {isLoading ? "Загрузка..." : "войти"}
+        {isLoading ? <BarLoader color="white" /> : "Вход"}
       </Button>
 
       {serverErrorMessage == "" ? null : (
